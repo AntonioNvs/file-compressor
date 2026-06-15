923dda66c9f70651b34e2c27acdbd102fbe576c3
AntonioNvs <tonim2612@gmail.com>
2026-06-14 21:38:31 -0300

    test: add unit tests for code generator

--- tests/unit/test_tree.py
+++ tests/unit/test_tree.py
@@ -1,5 +1,5 @@
 import pytest
-from src.huffman.tree import build_tree
+from src.huffman.tree import build_tree, generate_codes
 
 @pytest.mark.unit
 def test_single_char():
@@ -21,12 +21,10 @@ def test_two_chars():
     assert root.char is None
     assert root.freq == 3
     
-    # Left should be the one with lowest freq ('a')
     assert root.left is not None
     assert root.left.char == "a"
     assert root.left.freq == 1
     
-    # Right should be 'b'
     assert root.right is not None
     assert root.right.char == "b"
     assert root.right.freq == 2
@@ -43,3 +41,33 @@ def test_multiple_chars():
     
     assert root.left is not None
     assert root.right is not None
+
+@pytest.mark.unit
+def test_codes_are_prefix_free():
+    freqs = {"a": 5, "b": 9, "c": 12, "d": 13, "e": 16, "f": 45}
+    root = build_tree(freqs)
+    codes = generate_codes(root)
+    
+    for char1, code1 in codes.items():
+        for char2, code2 in codes.items():
+            if char1 != char2:
+                assert not code1.startswith(code2)
+
+@pytest.mark.unit
+def test_codes_unique():
+    freqs = {"a": 5, "b": 9, "c": 12, "d": 13, "e": 16, "f": 45}
+    root = build_tree(freqs)
+    codes = generate_codes(root)
+    
+    unique_codes = set(codes.values())
+    assert len(unique_codes) == len(codes)
+
+@pytest.mark.unit
+def test_variable_length():
+    freqs = {"a": 1, "b": 10, "c": 100, "d": 1000}
+    root = build_tree(freqs)
+    codes = generate_codes(root)
+    
+    # "d" tem maior frequência, deve ter o menor ou igual código em relação aos outros
+    # E "a" deve ser maior ou igual a "d" (com certeza será maior num caso tão extremo)
+    assert len(codes["d"]) < len(codes["a"])
